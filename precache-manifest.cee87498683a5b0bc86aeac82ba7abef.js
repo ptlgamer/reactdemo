c7f3a44059e2d2315768205936d575c104b5804a
phamthanhloc <phamthanhloc260194@gmail.com>
2018-11-15 00:20:20 +0700

    Updates

---
+++ precache-manifest.cee87498683a5b0bc86aeac82ba7abef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactdemo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "05f9f01e3b54b5245ef4",
+    "url": "/reactdemo/static/js/runtime~main.05f9f01e.js"
+  },
+  {
+    "revision": "417fe1892beeb8eb7cd3",
+    "url": "/reactdemo/static/js/main.417fe189.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/reactdemo/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "417fe1892beeb8eb7cd3",
+    "url": "/reactdemo/static/css/main.f8793e3e.chunk.css"
+  },
+  {
+    "revision": "f81504bd55bdeb287944f3e6a1b611e7",
+    "url": "/reactdemo/index.html"
+  }
+];
