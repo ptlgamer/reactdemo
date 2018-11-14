174fdda66aa6a73091c7db60935249063dab1ee3
phamthanhloc <phamthanhloc260194@gmail.com>
2018-11-15 00:33:14 +0700

    Updates

---
+++ precache-manifest.3a01a897b7b92ecbf4b021c78df7f1c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "05f9f01e3b54b5245ef4",
+    "url": "/reactdemo/static/js/runtime~main.05f9f01e.js"
+  },
+  {
+    "revision": "e5913638e33827cfc5d8",
+    "url": "/reactdemo/static/js/main.e5913638.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/reactdemo/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "4d4c75a52e2eb18698991fb1cc45be1c",
+    "url": "/reactdemo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/reactdemo/precache-manifest.cee87498683a5b0bc86aeac82ba7abef.js"
+  "/reactdemo/precache-manifest.3a01a897b7b92ecbf4b021c78df7f1c9.js"
 );
 
 workbox.clientsClaim();
